90a002e448720e7e150dfa6fa064ad9ed834af66
aLexa163-JV <alekcei-y@ya.ru>
2024-08-06 17:06:09 +0400

    исправил

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,3 @@
-/*
-Мы сканер не проходили,
-в интернете как то попалось, решил тут применить.
-Надеюсь проверять будет интереснее и приятнее
-Правда как это работает не очень понял (но вроде работает)
-*/
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         task1();
@@ -17,15 +9,8 @@ public class Main {
         task7();
     }
     public static void task1(){
-    /*
-    С помощью условного оператора напишите программу, которая выводит в консоль:
-    сообщение о том, что человеку 18 или больше лет;
-    сообщение, что возраст совершеннолетия еще не наступил, нужно немного подождать.
-    */
-        System.out.println("Задача 1");
-        System.out.print("Введите возраст:");
-        Scanner ageHuman = new Scanner (System.in);
-        byte age = ageHuman.nextByte();
+
+        byte age = 15;
         if ( age >= 18 ) {
             System.out.println ("Если возраст человека равен " + age + ", то он совершеннолетний.");
         }else {
@@ -34,16 +19,8 @@ public class Main {
     }
 
     public static void task2(){
-    /*
-    Человек одевается в зависимости от температуры воздуха.
-    Напишите программу, которая выводит сообщение:
-    «На улице холодно, нужно надеть шапку» — если температура ниже 5 градусов.
-    «Сегодня тепло, можно идти без шапки» — если температура воздуха выше 5 градусов.
-    */
-        System.out.println("Задача 2");
-        System.out.print("Введите градусы:");
-        Scanner outdoorTemperature = new Scanner(System.in);
-        byte degrees = outdoorTemperature.nextByte();
+
+        byte degrees = 6;
         if ( degrees < 5 ){
             System.out.println("На улице " + degrees + " градусов, нужно надеть шапку.");
         }else {
@@ -52,16 +29,8 @@ public class Main {
     }
 
     public static void task3(){
-    /*
-    За превышение скорости водителю могут выписать штраф.
-    Напишите программу, которая сообщает водителю о том, что:
-    скорость превышена, если она больше 60 км/ч;
-    превышения скорости нет, если она меньше 60 км/ч.
-    */
-        System.out.println("Задача 3");
-        System.out.print("Введите скорость:");
-        Scanner speedCar = new Scanner(System.in);
-        byte speed = speedCar.nextByte();
+
+        byte speed = 50;
         if ( speed > 60 ){
             System.out.println("Если скорость " + speed + " км/ч, то придется заплатить штраф.");
         }else {
@@ -70,18 +39,8 @@ public class Main {
     }
 
     public static void task4(){
-    /*
-    Напишите программу, которая помогает определить,
-    в какое учреждение нужно отправить человека в зависимости от его возраста:
-    Если человеку от 2 до 6 лет, то ему нужно ходить в детский сад.
-    Если человеку от 7 до 17 лет, то ему нужно ходить в школу.
-    Если человеку больше или равно 18 лет, но меньше или равно 24, то его место в университете.
-    А если человеку больше 24, то ему пора ходить на работу.
-    */
-        System.out.println("Задача 4");
-        System.out.print ("Введите возраст:");
-        Scanner agePerson = new Scanner(System.in);
-        byte age = agePerson.nextByte();
+
+        byte age = 15;
         if ( age > 2 && age <= 6 ){
             System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в детский сад.");
         } else if ( age >= 7 && age <= 17 ) {
@@ -94,37 +53,20 @@ public class Main {
     }
 
     public static void task5(){
-    /*
-    Как правило, на аттракционах действуют ограничения для детей по возрасту:
-    Если ребенку меньше 5 лет, то он не может кататься на аттракционе.
-    Если ребенку больше 5, но меньше 14 лет, то он может кататься только
-    в сопровождении взрослого. Если взрослого нет, то кататься нельзя.
-    Если ребенок старше 14 лет, то он может кататься без сопровождения взрослого.
-    */
-        System.out.println("Задача 5");
-        System.out.print("Введите возраст:");
-        Scanner ageAttraction = new Scanner(System.in);
-        byte age = ageAttraction.nextByte();
+
+        byte age = 12;
         if ( age < 5 ){
             System.out.println("Если возраст ребенка равен " + age + ", то ему нельзя кататься на аттракционе.");
         } else if ( age >= 5 && age < 14 ) {
             System.out.println("Если возраст ребенка равен " + age + ", то ему можно кататься на аттракционе в сопровождении взрослого.");
-        } else if ( age >=14 && age < 18 ) {
+        } else if (age >= 14) {
             System.out.println("Если возраст ребенка равен " + age + ", то ему можно кататься на аттракционе без сопровождения взрослого.");
-        } else if ( age >= 18 ) { // от себя добавил, для логики
-            System.out.println("Это уже не ребёнок)");
         }
     }
 
     public static void task6(){
-    /*
-    Вместимость одного вагона поезда — 102 человека.
-    Вагон рассчитан на 60 сидячих мест, все остальные — стоячие.
-    */
-        System.out.println("Задача 6");
-        System.out.print("Введите кол-во пассажиров:");
-        Scanner train = new Scanner(System.in);
-        int passengers = train.nextInt();
+
+        int passengers = 20;
         int total = 102;
         int sit = 60;
         int standing = total - sit;
@@ -138,24 +80,10 @@ public class Main {
     }
 
     public static void task7(){
-    /*
-    Даны три числа:
-    int one;
-    int two;
-    int three;
-    С помощью условного оператора и конструкции else напишите программу,
-    которая вычисляет, какое из трех чисел бо́льшее, и выводит результат в консоль.
-     */
-        System.out.println("Задача 7");
-        System.out.print("Введите первое число:");
-        Scanner one1 = new Scanner(System.in);
-        int one = one1.nextInt();
-        System.out.print("Введите второе число:");
-        Scanner two2 = new Scanner(System.in);
-        int two = two2.nextInt();
-        System.out.print("Введите третье число:");
-        Scanner three3 = new Scanner(System.in);
-        int three = three3.nextInt();
+
+        int one = 5;
+        int two = 7;
+        int three = 12;
         if ( one > two && one > three ){
             System.out.println("Число " + one + " больше других чисел.");
         } else if ( two > one && two > three ) {
